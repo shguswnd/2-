268325aef35886556ad63fa6c8882f3252bf73ba
shguswnd <nhj6336@gmail.com>
2022-09-19 00:25:55 +0900

    project

---
+++ src/Common/ReadingroomFactory.java
@@ -0,0 +1,14 @@
+package Common;
+
+import User.User;
+
+// 클래스 집합
+
+public class ReadingroomFactory extends returnmessage{
+	public User getUser() {
+		return new User();
+	}
+	public void createRoom(String type) {
+		
+	}
+}
